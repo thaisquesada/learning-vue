3ac3452a798cd777622dc371fb29e6b75b2ee674
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-15 11:38:06 -0300

    inputs e data binding 🐱‍💻

---
+++ instalacao_vue/src/app.js
@@ -0,0 +1,22 @@
+const MyNameApp = {
+    data() {
+        return {
+            name: "",
+            lastName: "Quesada",
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDefault();
+
+            console.log(this.input_name);
+
+            this.name = this.input_name;
+
+        }
+    }
+}
+
+Vue.createApp(MyNameApp).mount("#app");
