7c270104aae502bf1ec1684acdb12914c3ae933f
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-15 16:50:45 -0300

    reutilização de componentes 🐱‍💻

--- projeto_vue/src/components/Avatar.vue
+++ projeto_vue/src/components/Avatar.vue
@@ -1,15 +1,24 @@
 <template>
+    <div>
     <img :src="avatar" :alt="descricao">
+    <Reutilizacao />
+    </div>
 </template>
 
 <script>
+import Reutilizacao from './Reutilizacao.vue';
+
 export default {
-    name: 'Avatar',
+    name: "Avatar",
+    components: {
+        Reutilizacao
+    },
     data() {
         return {
-            avatar: '/img/avatar.png',
-            descricao: 'Thais Quesada'
-        }
-    }
+            avatar: "/img/avatar.png",
+            descricao: "Thais Quesada"
+        };
+    },
+    components: { Reutilizacao }
 }
 </script>
