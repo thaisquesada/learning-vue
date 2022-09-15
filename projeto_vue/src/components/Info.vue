f66e91250d168dc499e7d0c065d35b05a98ef308
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-15 14:24:41 -0300

    diretivas (v-if, v-show) 🐱‍💻

--- projeto_vue/src/components/Info.vue
+++ projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades.</p>
         <p>Utilizo as seguintes tecnologias:</p>
             <ul>
                 <li>JavaScript</li>
                 <li>C#</li>
                 <li>Java</li>
             </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: true,
+            mostrar_email: true,
+            email: 'contato@email.com '
+        }
+    }
 }
 </script>
