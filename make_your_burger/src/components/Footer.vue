eb6dbea8e2d608f22f2d5cf0eb80ff01c0fc45ce
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-16 10:34:22 -0300

    estilização (header e footer) 🐱‍💻

--- make_your_burger/src/components/Footer.vue
+++ make_your_burger/src/components/Footer.vue
@@ -1,9 +1,24 @@
 <template>
-    <p>Footer</p>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2022</p>
+  </footer>
 </template>
 
 <script>
-    export default {
-        name: "Footer"
-    }
+export default {
+  name: "Footer",
+};
 </script>
+
+<style scoped>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
