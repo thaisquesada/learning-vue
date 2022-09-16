fe7a107410ce9ca953220d99489b4a2bd00236ca
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-09-16 17:47:47 -0300

    atualização de pedidos (patch) 🐱‍💻

--- make_your_burger/src/components/Navbar.vue
+++ make_your_burger/src/components/Navbar.vue
@@ -1,18 +1,20 @@
 <template>
-  <nav id="nav">
-    <router-link to="/" id="logo-url">
-        <img :src="logo" :alt="alt" id="logo">
-    </router-link>
-    <router-link to="/">Home</router-link>
-    <router-link to="/pedidos">Pedidos</router-link>
-  </nav>
+  <div>
+    <div id="nav">
+      <router-link id="logo-url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Fazer Pedido</router-link>
+      <router-link to="/pedidos">Pedidos</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Navbar",
-        props: ["logo", "alt"]
-    }
+export default {
+  name: "Navbar",
+  props: ["logo", "alt"]
+}
 </script>
 
 <style scoped>
@@ -24,25 +26,21 @@
     justify-content: flex-end;
     align-items: center;
   }
-
   #nav #logo-url {
     margin: auto;
     margin-left: 0;
   }
-
   #logo {
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
   }
-
   #nav a {
-    color: #FCBA03;
+    color: #fcba03;
     text-decoration: none;
     margin: 12px;
     transition: .5s;
   }
-
   #nav a:hover {
-    color: #fff;
+    color: #FFF;
   }
 </style>
